ab04a2da7c55451d6451467e4fd317cd7e8af95b
Hefezopf <info@hopf-it.de>
2014-10-31 18:33:20 +0100

    inint

--- src/_de/hopfit/util/InitServlet.java
+++ src/_de/hopfit/util/InitServlet.java
@@ -23,32 +23,36 @@ import de.hopfit.timer.TarifListener;
 public class InitServlet extends HttpServlet{
 
 	private static final long serialVersionUID = 1L;
-	
+
 	public InitServlet() {
 		super();
-		Logger.log("InitServlet()");	
+		Logger.log("InitServlet()");
 	}
-		
+
 	  public void init() {
-		Logger.log("InitServlet init()");	
-	    InvoiceListener il = new InvoiceListener();
-	    TarifListener tl = new TarifListener();	 
-	    DeployWSListener dWSl = new DeployWSListener();	 
-	    StatistikListener sl = new StatistikListener();	 
-	    // Globale variabeln aus der const.java initialisieren
-	    Const.invoiceTimer = new ContextThread(il, Const.TIMER_INTERVAL/2);
-	    Const.tarifTimer = new ContextThread(tl, Const.TIMER_INTERVAL*2);
-	    Const.deployWSTimer = new ContextThread(dWSl, Const.TIMER_INTERVAL/1);
-	    Const.statistikTimer = new ContextThread(sl, Const.TIMER_INTERVAL/3);
-    	Const.invoiceTimer.start();
-    	Const.tarifTimer.start();
-    	Const.deployWSTimer.start();
-    	Const.statistikTimer.start();
+		Logger.log("InitServlet init()");
+
+		System.out.println("Listner auskommentiert!!!");
+//	    InvoiceListener il = new InvoiceListener();
+//	    TarifListener tl = new TarifListener();
+//	    DeployWSListener dWSl = new DeployWSListener();
+//	    StatistikListener sl = new StatistikListener();
+//	    // Globale variabeln aus der const.java initialisieren
+//	    Const.invoiceTimer = new ContextThread(il, Const.TIMER_INTERVAL/2);
+//	    Const.tarifTimer = new ContextThread(tl, Const.TIMER_INTERVAL*2);
+//	    Const.deployWSTimer = new ContextThread(dWSl, Const.TIMER_INTERVAL/1);
+//	    Const.statistikTimer = new ContextThread(sl, Const.TIMER_INTERVAL/3);
+//    	Const.invoiceTimer.start();
+//    	Const.tarifTimer.start();
+//    	Const.deployWSTimer.start();
+//    	Const.statistikTimer.start();
+		System.out.println("Listner auskommentiert!!!");
+
 		new Const();
 	 }
-        				  
+
 	  public void destroy() {
-		Logger.log("InitServlet destroy()");		 
+		Logger.log("InitServlet destroy()");
 	  	Const.invoiceTimer.interrupt();
 	  	Const.tarifTimer.interrupt();
 	  	Const.deployWSTimer.interrupt();
